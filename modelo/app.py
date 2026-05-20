f348b4a8c8c2830ad9caf648b64d463829c0cd38
Kaique Wilson <kaiquewilson94@gmail.com>
2026-05-20 11:30:41 -0300

    Possibilidade de aplicar desconto

--- modelo/app.py
+++ modelo/app.py
@@ -3,8 +3,14 @@ from cardapio.prato import Prato
 from cardapio.bebida import Bebida
 
 restaurante1 = Restaurante("Sabor Express", "Comida Rápida", status=True)
-bebida1 = Bebida("Refrigerante", 5.00, ' grande')
-prato1 = Prato("Hambúrguer", 15.00, 'Hambúrguer')
+
+
+bebida1 = Bebida("Refrigerante", 5.00, '500ml')
+bebida1.aplicar_desconto()
+prato1 = Prato("Hambúrguer", 15.00, "Hambúrguer artesanal com queijo, alface e tomate")
+prato1.aplicar_desconto()
+
+
 restaurante1.adicionar_item_cardapio(bebida1)
 restaurante1.adicionar_item_cardapio(prato1)    
 
--- modelo/cardapio/item_cardapio.py
+++ modelo/cardapio/item_cardapio.py
@@ -1,5 +1,12 @@
-class ItemCardapio:
+from abc import ABC, abstractmethod
+
+class ItemCardapio(ABC):
     def __init__(self, nome, preco):
         self._nome = nome
         self._preco = preco
         #self._descricao = descricao
+
+
+    @abstractmethod
+    def aplicar_desconto(self):
+        pass
